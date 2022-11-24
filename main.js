9e9723c50646359f55ba66309ebf18d9652e770a
melv <69640352+cmelv@users.noreply.github.com>
2022-11-24 16:34:26 +0000

    basic game logic

--- main.js
+++ main.js
@@ -1,20 +1,58 @@
 let computerChoice;
 
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+
 
 
 function getComputerChoice(){
     computerChoice = Math.floor(Math.random()*3)+1;
-    console.log(computerChoice)
+    /*console.log(computerChoice)*/
     if (computerChoice === 1){
         computerChoice = "Rock";
     }else if (computerChoice === 2){
         computerChoice = "Paper";
     }else computerChoice = "Scissors";
-    console.log(computerChoice)
+    /*console.log(computerChoice)*/
     return computerChoice
 
 }
 
 function playRound(playerSelection,computerSelection){
-    return
+    /*console.log(playerSelection)*/
+    /*console.log(computerSelection)*/
+    if(playerSelection === computerSelection){
+        result = "Its a Draw"
+        /*console.log(result);*/
+        return result
+    }
+
+    if (playerSelection === "paper"  && computerSelection === "Rock"){
+        result = "You win! " + playerSelection + " beats " + computerSelection;
+        /*console.log(result);*/
+        return result
+    }
+
+    if (playerSelection === "Rock"  && computerSelection === "Scissors"){
+        result = "You win! " + playerSelection + " beats " + computerSelection;
+        /*console.log(result);*/
+        return result
+    }
+
+    if (playerSelection === "Scissors"  && computerSelection === "Paper"){
+        result = "You win! " + playerSelection + " beats " + computerSelection;
+        /*console.log(result);*/
+        return result
+    }else {
+        result = "You Lose! " + computerSelection + " beats " + playerSelection;
+        /*console.log(result);*/
+        return result;
+    }
+    
+
+
+    
+    
 }
+
+playRound(playerSelection,computerSelection)
